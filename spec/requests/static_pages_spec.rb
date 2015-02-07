3466b4f36c7988c40838b43c7b02596f2e6a3d95
syunta <kitagawa@timedia.co.jp>
2015-02-07 17:13:35 +0900

    Add spec for help contents

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,9 +1,20 @@
 require 'spec_helper'
 
-describe "Home page" do
+describe "Static pages" do
 
-  it "should have the content 'Sample App'" do
-    visit '/static_pages/home'
-    expect(page).to have_content('Sample App')
+  describe "Home page" do
+
+    it "should have the content 'Sample App'" do
+      visit '/static_pages/home'
+      expect(page).to have_content('Sample App')
+    end
+  end
+
+  describe "Help page" do
+
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_content('Help')
+    end
   end
 end
