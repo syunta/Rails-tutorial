9ff4b41afd8dac554a002a3542f777b13a3e828f
syunta <kitagawa@timedia.co.jp>
2015-02-07 18:24:17 +0900

    Answer 3.5.2

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+  let(:base_title) { "RailsTutorial" }
+
   describe "Home page" do
 
     it "should have the content 'Sample App'" do
@@ -11,7 +13,7 @@ describe "Static pages" do
 
     it "should have the right title" do
       visit '/static_pages/home'
-      expect(page).to have_title("RailsTutorial | Home")
+      expect(page).to have_title("#{base_title} | Home")
     end
   end
 
@@ -24,7 +26,7 @@ describe "Static pages" do
 
     it "should have the right title" do
       visit '/static_pages/help'
-      expect(page).to have_title("RailsTutorial | Help")
+      expect(page).to have_title("#{base_title} | Help")
     end
   end
 
@@ -37,7 +39,7 @@ describe "Static pages" do
 
     it "should have the right title" do
       visit '/static_pages/about'
-      expect(page).to have_title("RailsTutorial | About")
+      expect(page).to have_title("#{base_title} | About")
     end
   end
 
@@ -50,7 +52,7 @@ describe "Static pages" do
 
     it "should have the right title" do
       visit '/static_pages/contact'
-      expect(page).to have_title("RailsTutorial | Contact")
+      expect(page).to have_title("#{base_title} | Contact")
     end
   end
 end
