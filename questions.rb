72f03dece39193466a9b7f876faa347dda092209
syunta <kitagawa@timedia.co.jp>
2015-02-07 22:22:03 +0900

    Answer 4.6.3

--- questions.rb
+++ questions.rb
@@ -11,3 +11,31 @@ class String
     self.split('').shuffle.join
   end
 end
+
+# 3
+
+def check
+  person1 = {
+    :first => 'syunta',
+    :last => 'kitagawa'
+  }
+  
+  person2 = {
+    :first => 'foo',
+    :last => 'bar'
+  }
+  
+  person3 = {
+    first: 'hoge',
+    last: 'huga'
+  }
+
+  params = {}
+  params[:father] = person1
+  params[:mother] = person2
+  params[:child] = person3
+
+  puts params[:father][:first]
+  puts params[:mother][:first]
+  puts params[:child][:last]
+end
