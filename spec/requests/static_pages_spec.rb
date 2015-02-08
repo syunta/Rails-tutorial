db4d6907474ad090cf4a5dcb89b4c70dd27a88f1
syunta <kitagawa@timedia.co.jp>
2015-02-08 21:53:18 +0900

    Refactor spec using before

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,58 +5,53 @@ describe "Static pages" do
   let(:base_title) { "RailsTutorial" }
 
   describe "Home page" do
+    before { visit root_path }
 
     it "should have the content 'Sample App'" do
-      visit root_path
       expect(page).to have_content('Sample App')
     end
 
     it "should have the right title" do
-      visit root_path
       expect(page).to have_title("#{base_title}")
     end
 
     it "should not have a custom page title" do
-      visit root_path
       expect(page).not_to have_title('| Home')
     end
   end
 
   describe "Help page" do
+    before { visit help_path }
 
     it "should have the content 'Help'" do
-      visit help_path
       expect(page).to have_content('Help')
     end
 
     it "should have the right title" do
-      visit help_path
       expect(page).to have_title("#{base_title} | Help")
     end
   end
 
   describe "About page" do
+    before { visit about_path }
 
     it "should have the content 'About Us'" do
-      visit about_path
       expect(page).to have_content('About Us')
     end
 
     it "should have the right title" do
-      visit about_path
       expect(page).to have_title("#{base_title} | About")
     end
   end
 
   describe "Contact page" do
+    before { visit contact_path }
 
     it "should have the content 'Contact Us'" do
-      visit contact_path
       expect(page).to have_content('Contact Us')
     end
 
     it "should have the right title" do
-      visit contact_path
       expect(page).to have_title("#{base_title} | Contact")
     end
   end
