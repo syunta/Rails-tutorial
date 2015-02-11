3f2d0be688b8842b1b35477181a7f6ae6c4de981
syunta <kitagawa@timedia.co.jp>
2015-02-11 21:12:10 +0900

    Add spec verify User attributes

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,11 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before { @user = User.new(name: 'syunta', email: 'syunta@example.com') }
+
+  subject { @user }
+
+  it { should respond_to(:name) }
+  it { should respond_to(:email) }
 end
