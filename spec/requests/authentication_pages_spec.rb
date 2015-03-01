ca62944d29361975432cf806e369805e5983e9e4
syunta <kitagawa@timedia.co.jp>
2015-03-01 13:59:00 +0900

    Add spec invalid signin

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -6,7 +6,11 @@ describe "Authentication" do
   describe "signin page" do
     before { visit signin_path }
 
-    it { should have_content('Sign in') }
-    it { should have_title('Sign in') }
+    describe "with invalid information" do
+      before { click_button "Sign in" }
+
+      it { should have_content('Sign in') }
+      it { should have_selector('div.alert.alert-error', text: 'invalid') }
+    end
   end
 end
