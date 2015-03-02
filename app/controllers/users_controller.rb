20baa254c61dc81342c0ebdcbbd2084b0c46cb44
syunta <kitagawa@timedia.co.jp>
2015-03-02 23:03:47 +0900

    Add link of Settings at signin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,6 +22,9 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def update
+  end
+
   private
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
