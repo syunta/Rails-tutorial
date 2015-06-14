2b7acd898697cef25d7c1ded92c0fd9c26a2f7d3
syunta <kitagawa@timedia.co.jp>
2015-06-14 18:16:00 +0900

    Add spec for partial of following/followers count

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -56,6 +56,17 @@ describe "Static pages" do
           end
         end
 
+        describe "follower/following" do
+          let(:other_user) { FactoryGirl.create(:user) }
+          before do
+            other_user.follow!(user)
+            visit root_path
+          end
+
+          it { should have_link("0 following", href: following_user_path(user)) }
+          it { should have_link("1 followers", href: followers_user_path(user)) }
+        end
+
         describe "pagination" do
           before do
             31.times { FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum") }
