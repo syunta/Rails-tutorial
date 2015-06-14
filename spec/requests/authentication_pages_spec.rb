2eab68af01469aefe8ae541a367ef0b441e0a336
syunta <kitagawa@timedia.co.jp>
2015-06-14 20:04:45 +0900

    Fix typo

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -99,7 +99,7 @@ describe "Authentication" do
           specify { expect(response).to redirect_to(signin_path) }
         end
 
-        describe "submitting to the create action" do
+        describe "submitting to the destroy action" do
           before { delete micropost_path(FactoryGirl.create(:micropost)) }
           specify { expect(response).to redirect_to(signin_path) }
         end
